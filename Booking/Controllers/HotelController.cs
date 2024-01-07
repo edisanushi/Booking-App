502194e842988fd9546fbb0cc3ea530ee8c82bb3
edisanushi <edisanushi@gmail.com>
2024-01-07 17:24:48 +0100

    first-commit

---
+++ Booking/Controllers/HotelController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Booking.Controllers
+{
+	public class HotelController: Controller
+	{
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult HotelBooking()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult HotelChain()
+        {
+            return View();
+        }
+
+   
+        public IActionResult RoomDetail()
+        {
+            return View();
+        }
+        public IActionResult Resort()
+        {
+            return View();
+        }
+        public IActionResult HotelDetail()
+        {
+            return View();
+        }
+        public IActionResult HotelGrid()
+        {
+            return View();
+        }
+        public IActionResult HotelList()
+        {
+            return View();
+        }
+
+
+
+
+    }
+
+}
+
+
+
+
---
+++ Booking/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Booking.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
